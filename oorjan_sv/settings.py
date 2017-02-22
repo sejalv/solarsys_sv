26efdb510911daac060d9df68b17fbfdc511d18c
Sejal V <sejal.v18@gmail.com>
2017-02-22 08:54:25 +0530

    Update settings.py

--- oorjan_sv/settings.py
+++ oorjan_sv/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'solarsys',
-        'USER': 'postgres',
-        'PASSWORD': '4321',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
